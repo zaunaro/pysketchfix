d53231fc11005279964c4d04c53616c2c5b8e8e6
Daniel <zaunerdan@fim.uni-passau.de>
2020-12-29 17:43:13 +0100

    Now the project folder must only be modified on one place

--- PySketchFix/_PYSKETCHFIX.py
+++ PySketchFix/_PYSKETCHFIX.py
@@ -7,21 +7,22 @@ import SketchMaker
 
 # Here you have to configure your Python Interpreter with which you want to run the process.
 python_interpreter = "/Users/daniel/BachelorarbeitDZ/PySketchFix/venv/bin/python"
+project_folder_location = "/Users/daniel/BachelorarbeitDZ/PySketchFix/"
 
 # The location where the sketch generator, ochiai and the patch finder are located.
-sketch_generator = "/Users/daniel/BachelorarbeitDZ/PySketchFix/SketchMaker.py"
-patch_finder = "/Users/daniel/BachelorarbeitDZ/PySketchFix/PatchFinder.py"
+sketch_generator = project_folder_location + "SketchMaker.py"
+patch_finder = project_folder_location + "PatchFinder.py"
 
 # Debuggers which are possible:
-fault_localizer = "/Users/daniel/BachelorarbeitDZ/PySketchFix/FaultLocalizer.py"
+fault_localizer = project_folder_location + "FaultLocalizer.py"
 OCHIAI = "ochiai"
 TARANTULA = "tarantula"
 
 # The buggy lines of the maximum bug file.
 maximum_buggy_lines = "4,5"
-maximum_bug_file = "/Users/daniel/BachelorarbeitDZ/PySketchFix/BugFile/maximum/maximum.py"
-maximum_bug_file_test = "/Users/daniel/BachelorarbeitDZ/PySketchFix/BugFile/maximum/testsuite/test_maximum.py"
-maximum_test_input = "/Users/daniel/BachelorarbeitDZ/PySketchFix/BugFile/maximum/tests_for_fault_localization/input_tests.txt"
+maximum_bug_file = project_folder_location + "BugFile/maximum/maximum.py"
+maximum_bug_file_test = project_folder_location + "BugFile/maximum/testsuite/test_maximum.py"
+maximum_test_input = project_folder_location + "BugFile/maximum/tests_for_fault_localization/input_tests.txt"
 # buggy_lines = maximum_buggy_lines
 # bug_file = maximum_bug_file
 # bug_file_test = maximum_bug_file_test
@@ -29,9 +30,9 @@ maximum_test_input = "/Users/daniel/BachelorarbeitDZ/PySketchFix/BugFile/maximum
 
 # The buggy lines of the minimum bug file.
 minimum_buggy_lines = "4,5"
-minimum_bug_file = "/Users/daniel/BachelorarbeitDZ/PySketchFix/BugFile/minimum/minimum.py"
-minimum_bug_file_test = "/Users/daniel/BachelorarbeitDZ/PySketchFix/BugFile/minimum/testsuite/test_minimum.py"
-minimum_test_input = "/Users/daniel/BachelorarbeitDZ/PySketchFix/BugFile/minimum/tests_for_fault_localization/input_tests.txt"
+minimum_bug_file = project_folder_location + "BugFile/minimum/minimum.py"
+minimum_bug_file_test = project_folder_location + "BugFile/minimum/testsuite/test_minimum.py"
+minimum_test_input = project_folder_location + "BugFile/minimum/tests_for_fault_localization/input_tests.txt"
 # buggy_lines = minimum_buggy_lines
 # bug_file = minimum_bug_file
 # bug_file_test = minimum_bug_file_test
@@ -39,9 +40,9 @@ minimum_test_input = "/Users/daniel/BachelorarbeitDZ/PySketchFix/BugFile/minimum
 
 # The buggy lines of the is prime bug file.
 is_prime_buggy_lines = "6,5,3"
-is_prime_bug_file = "/Users/daniel/BachelorarbeitDZ/PySketchFix/BugFile/is_prime/is_prime.py"
-is_prime_bug_file_test = "/Users/daniel/BachelorarbeitDZ/PySketchFix/BugFile/is_prime/testsuite/test_is_prime.py"
-is_prime_test_input = "/Users/daniel/BachelorarbeitDZ/PySketchFix/BugFile/is_prime/tests_for_fault_localization/input_tests.txt"
+is_prime_bug_file = project_folder_location + "BugFile/is_prime/is_prime.py"
+is_prime_bug_file_test = project_folder_location + "BugFile/is_prime/testsuite/test_is_prime.py"
+is_prime_test_input = project_folder_location + "BugFile/is_prime/tests_for_fault_localization/input_tests.txt"
 buggy_lines = is_prime_buggy_lines
 bug_file = is_prime_bug_file
 bug_file_test = is_prime_bug_file_test
@@ -49,12 +50,12 @@ test_input = is_prime_test_input
 
 # The buggy lines of the positive indicator function.
 positive_indicator_buggy_lines = "2,3"
-positive_indicator_bug_file = "/Users/daniel/BachelorarbeitDZ/PySketchFix/BugFile/positive_indicator/" \
-                              "positive_indicator.py"
-positive_indicator_bug_file_test = "/Users/daniel/BachelorarbeitDZ/PySketchFix/BugFile/positive_indicator/testsuite/" \
-                                   "test_positive_indictator.py"
-positive_indicator_test_input = "/Users/daniel/BachelorarbeitDZ/PySketchFix/BugFile/tests_for_fault_localization/tests_for_" \
-                                "ochiai/input_tests.txt"
+positive_indicator_bug_file = project_folder_location + "BugFile/positive_indicator/" \
+                                                         "positive_indicator.py"
+positive_indicator_bug_file_test = project_folder_location + "BugFile/positive_indicator/testsuite/" \
+                                                              "test_positive_indictator.py"
+positive_indicator_test_input = project_folder_location + "BugFile/tests_for_fault_localization/tests_for_" \
+                                                           "ochiai/input_tests.txt"
 # buggy_lines = positive_indicator_buggy_lines
 # bug_file = positive_indicator_bug_file
 # bug_file_test = positive_indicator_bug_file_test
@@ -62,9 +63,9 @@ positive_indicator_test_input = "/Users/daniel/BachelorarbeitDZ/PySketchFix/BugF
 
 # The buggy lines of the mid bug file.
 mid_buggy_lines = "6,2,1"
-mid_bug_file = "/Users/daniel/BachelorarbeitDZ/PySketchFix/BugFile/mid/mid.py"
-mid_bug_file_test = "/Users/daniel/BachelorarbeitDZ/PySketchFix/BugFile/mid/testsuite/test_mid.py"
-mid_test_input = "/Users/daniel/BachelorarbeitDZ/PySketchFix/BugFile/mid/tests_for_ochiai/input_tests.txt"
+mid_bug_file = project_folder_location + "BugFile/mid/mid.py"
+mid_bug_file_test = project_folder_location + "BugFile/mid/testsuite/test_mid.py"
+mid_test_input = project_folder_location + "BugFile/mid/tests_for_ochiai/input_tests.txt"
 # buggy_lines = mid_buggy_lines
 # bug_file = mid_bug_file
 # bug_file_test = mid_bug_file_test
